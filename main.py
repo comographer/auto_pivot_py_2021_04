f7bc018e36a713c74dac9f6fb4a6c9847637e990
Geunmo Kim <kimgeunmo@gmail.com>
2021-04-21 20:40:06 +0200

    korea pivot in progress

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import pandas as pd
-import os
 
-df = pd.read_excel
+
+korea = "korea.xlsx"
+
+df = pd.read_excel(korea, engine="openpyxl")
+
+pivoted = df.pivot(
+    columns=[
+        "Sold-to-party name",
+        "Customer PO no.",
+        "Sales order no",
+        "Material",
+        "Size",
+        "Pattern",
+        "Brand",
+        "F.Dest text",
+        "R.EPC date",
+        "CY date",
+        "On-board date",
+        "ETA date",
+    ],
+    values=["SO qty", "Confirm qty", "CTR measure"],
+)
+
+pivoted.to_excel("korea_pivot.xlsx")
